8b07308114e2b1c9a94b9773a48332d4c00b2907
makerlove1 <jakeworks800@gmail.com>
2026-03-09 23:05:18 +0800

    Minimal build config: simplify vue.config.js and add memory optimization

--- vue.config.js
+++ vue.config.js
@@ -1,43 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: './',
   outputDir: 'dist',
-  assetsDir: 'static',
-  transpileDependencies: [],
-  
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    }
-  },
-  
-  pwa: {
-    name: 'BrightKidz',
-    shortName: 'BrightKidz',
-    themeColor: '#667eea',
-    msTileColor: '#667eea',
-    
-    manifestOptions: {
-      name: 'BrightKidz - Educational Games for Kids',
-      short_name: 'BrightKidz',
-      description: 'Fun educational games for children',
-      theme_color: '#667eea',
-      background_color: '#1a1a2e',
-      display: 'standalone',
-      start_url: './',
-      icons: [
-        {
-          src: './img/logo.svg',
-          sizes: '512x512',
-          type: 'image/svg+xml'
-        }
-      ]
-    },
-    
-    workboxOptions: {
-      skipWaiting: true,
-      clientsClaim: true
-    }
-  }
+  transpileDependencies: []
 };
